8b07976365024eaa6b5fda614b533b46fe8bd666
Bestwo <amine.mostefa2003@gmail.com>
2024-09-06 14:17:37 +0200

    Patrick

---
+++ contact.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2 class="contact">CONTACTEZ NOUS</h2>
+    <p class="message_contact"><b>Envoyez-nous un message à bonjour@dogshop.fr</b></p>
+    <a href="index.php" class="achetez"><b>Achetez dès maintenant</b></a>
+
+</body>
+</html>
